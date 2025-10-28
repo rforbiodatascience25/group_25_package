d3a40d74979e43760a4b79a29c171d935291abeb
InfiniteLobster <szcholewinski@gmail.com>
2025-10-28 09:44:15 +0100

    Adding stuff

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+#' Codon Data
+#' Description
+#' This is file storing the data for the project.
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codo"
